501b10438a04c47673b38e493a60675ede9f1612
MAGICTNT <maxjavaux@gmail.com>
2024-09-16 14:58:20 +0200

    add debut logique gestion page

---
+++ src/main/java/fr/challenge/filerouge_utopios/controller/PageController.java
@@ -0,0 +1,55 @@
+package fr.challenge.filerouge_utopios.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    //    @Autowired
+    public PageController() {
+        // ici les init des service entre autre et decommenter l'anotation
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/profil")
+    public String profil() {
+//        if (loginService.isLogged()) {
+//            model.addAttribute("pseudo", loginService.getPseudo());
+//            return "profil";
+//        }
+        return "redirect:/login";
+    }
+
+    @RequestMapping("/tournaments")
+    public String tournaments() {
+//        if (loginService.isLogged()) {
+//            model.addAttribute("pseudo", loginService.getPseudo());
+//            return "tournaments";
+//        }
+        return "redirect:/login";
+    }
+
+    @RequestMapping("/tournament/id={idTournament}")
+    public String tournaments(@PathVariable int idTournament, Model model) {
+        //------- logique pour trouver un tournament par id -------//
+        //  Tournament tournamentFind tournamentService.getTournamentById(id)  //
+        //  if (tournamentFind != null){
+        //      model.addAttribute("tournament", tournamentFind);
+        //      return "tournament";
+        //  }
+        //
+        //
+        return "redirect:/login";
+    }
+
+
+}
