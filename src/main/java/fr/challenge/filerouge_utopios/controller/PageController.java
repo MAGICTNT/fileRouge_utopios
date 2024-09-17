a4edbfa72a6748c86ab3f10f6d4a48dcdb06d37b
Thibault Garcia <thibault.garcia.pro@gmail.com>
2024-09-17 15:14:02 +0200

    remove tournament logic from PageController

--- src/main/java/fr/challenge/filerouge_utopios/controller/PageController.java
+++ src/main/java/fr/challenge/filerouge_utopios/controller/PageController.java
@@ -1,20 +1,16 @@
 package fr.challenge.filerouge_utopios.controller;
 
-import fr.challenge.filerouge_utopios.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
+import fr.challenge.filerouge_utopios.service.LoginService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
+    private final LoginService loginService;
 
-    //    @Autowired
-    public PageController() {
-        // ici les init des service entre autre et decommenter l'anotation
+    public PageController(LoginService loginService) {
+        this.loginService = loginService;
     }
 
     @RequestMapping("/")
@@ -23,41 +19,10 @@ public class PageController {
     }
 
     @RequestMapping("/profil")
-    public String profil() {
-//        if (loginService.isLogged()) {
-//            model.addAttribute("pseudo", loginService.getPseudo());
-//            return "profil";
-//        }
+    public String profil(Model model) {
+        if (loginService.isLoggedIn()) {
+            return "profil";
+        }
         return "redirect:/login";
     }
-
-    @RequestMapping("/tournaments")
-    public String tournaments() {
-//        if (loginService.isLogged()) {
-//            model.addAttribute("pseudo", loginService.getPseudo());
-//            return "tournaments";
-//        }
-        return "redirect:/login";
-    }
-
-    @RequestMapping("/tournament/id={idTournament}")
-    public String tournaments(@PathVariable int idTournament, Model model) {
-        //------- logique pour trouver un tournament par id -------//
-        //  Tournament tournamentFind tournamentService.getTournamentById(id)  //
-        //  if (tournamentFind != null){
-        //      model.addAttribute("tournament", tournamentFind);
-        //      return "tournament";
-        //  }
-        //
-        //
-        return "redirect:/login";
-    }
-
-    @RequestMapping("/inscription")
-    public String register(Model model) {
-        model.addAttribute("user", new User());
-        return "inscription-form";
-    }
-
-
 }
